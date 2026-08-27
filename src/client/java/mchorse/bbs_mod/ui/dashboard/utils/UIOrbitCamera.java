cdcfba5ed9c9a774e842fa3b13f629f93ac70e55
Matias Neto <mattux_@hotmail.com>
2026-08-27 00:54:23 -0300

    Fix: Third person transform

--- src/client/java/mchorse/bbs_mod/ui/dashboard/utils/UIOrbitCamera.java
+++ src/client/java/mchorse/bbs_mod/ui/dashboard/utils/UIOrbitCamera.java
@@ -1,6 +1,5 @@
 package mchorse.bbs_mod.ui.dashboard.utils;
 
-import mchorse.bbs_mod.BBSSettings;
 import mchorse.bbs_mod.camera.OrbitCamera;
 import mchorse.bbs_mod.ui.framework.UIContext;
 import mchorse.bbs_mod.ui.framework.elements.IUIElement;
@@ -37,11 +36,6 @@ public class UIOrbitCamera implements IUIElement
     @Override
     public IUIElement mouseClicked(UIContext context)
     {
-        if (this.control && BBSSettings.editorFlightFreeLook != null && BBSSettings.editorFlightFreeLook.get())
-        {
-            return null;
-        }
-
         int i = this.orbit.canStart(context);
 
         if (i >= 0)
